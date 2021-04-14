da48a6c22ad43b37205369fde4cbcc81ee00254f
Andrew Guernsey <>
2021-04-14 15:35:01 +0000

    Completed No. 6

--- main.py
+++ main.py
@@ -2,33 +2,10 @@ from Post import Post
 
 all_posts_archive = []
 
-Post("R2D2", "Bleep Bloop, Bloop Beep?")
-
 # your code here
-def printPost():
-    for post in all_posts_archive:
-        print(post)
-while True:
-    try:
-        option = int(
-            input("""
-    What would you like to do?
-    
-    1 - View posts
-    2 - Create posts
-    
-    9 - Quit
-    """))
-    except ValueError:
-        print("")
-        continue
-    if option == 1:
-        printPost()
-    elif option == 2:
-        continue
-    elif option == 9:
-        quit('User initiated Quit')
-        break
-    else:
-        print('Not an option!')
-        continue
+post1 = Post("Marie", "This is my first post!")
+post2 = Post("Shaq", "BLOCKED.")
+post3 = Post("R2D2", "BEEEEEP BOOOP BLEEP BLOOP BLOOP BLEEP!")
+print(post1)
+print(post2)
+print(post3)
